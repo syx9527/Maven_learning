1bc853e8973cd129a1102bf9a33d121bfc1482fb
syx9527 <1275241305@qq.com>
2023-01-18 17:27:39 +0800

    upload

---
+++ idea-maven-space/pro03-maven-import/src/main/java/com/shaoyx/maven/App.java
@@ -0,0 +1,12 @@
+package com.shaoyx.maven;
+
+/**
+ * Hello world!
+ *
+ * @author SYX
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
